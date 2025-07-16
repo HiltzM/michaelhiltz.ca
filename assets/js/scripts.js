806b88693065bcd29f9b710b414283d21ca04be3
HiltzM <hiltzmn@gmail.com>
2025-07-16 15:48:36 -0600

    Delete assets directory

--- assets/js/scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var menu = document.getElementById('nav-menu');
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
-    }
-}
